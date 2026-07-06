a6ffe22d55aecc849b86590e52b47eb685cefcbc
Luis Pinillos <102693827+lpinillos@users.noreply.github.com>
2026-07-06 13:54:15 -0500

    Add test comment in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import uuid
 import logging
 from flask import Flask, render_template, request, jsonify
 from azure.storage.blob import BlobServiceClient
-
+# test
 app = Flask(__name__)
 
 # Logging ──────────────────────────────────
